ab5769796a0e5941cf8fee69d5ed9e11dab09e25
brunoscholz <bruno@oction.com.br>
2017-09-26 13:42:10 -0300

    kinda backup

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=boilerplate',
+    'dsn' => 'mysql:host=localhost;dbname=dyntech2',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
